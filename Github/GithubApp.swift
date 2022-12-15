c77dfba660b307a9a6778a22c18173099a410db8
Camilo Rodriguez Gaviria <camilorod1@gmail.com>
2022-12-15 10:28:14 +0100

    Initial commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Camilo Rodriguez Gaviria on 2022-12-15.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
