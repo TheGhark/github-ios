1b168e8d5fb6cac16a92e4125f8635bf38d07933
Camilo Rodriguez <camilorod1@gmail.com>
2022-12-15 18:27:50 +0100

    Created RepositoryView

---
+++ Github/sources/views/repository/RepositoryView+Model.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension RepositoryView {
+    struct Model {
+        let name: String
+        let owner: String
+        let description: String?
+    }
+}
---
+++ Github/sources/views/repository/RepositoryView.swift
@@ -0,0 +1,50 @@
+import Foundation
+import SwiftUI
+
+struct RepositoryView: View {
+    // MARK: - Properties
+
+    private let model: Model
+
+    // MARK: - Initialization
+
+    init(model: Model) {
+        self.model = model
+    }
+
+    // MARK: - View
+
+    var body: some View {
+        HStack {
+            VStack {
+                HStack {
+                    Text(model.name)
+                        .font(.title2)
+                        .bold()
+                    Spacer()
+                    Text(model.owner)
+                        .font(.title3)
+                }
+                if let description = model.description {
+                    HStack {
+                        Text(description)
+                            .font(.callout)
+                        Spacer()
+                    }
+                }
+            }
+            Spacer()
+            Image(systemName: "chevron.right")
+                .foregroundColor(.black.opacity(0.5))
+        }
+        .padding([.leading, .trailing])
+    }
+}
+
+struct RepositoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        RepositoryView(model: .init(name: "FlexBot",
+                                    owner: "abhisheksisodia",
+                                    description: "FlexBot is chat bot built using Microsoft Bot Framework"))
+    }
+}
